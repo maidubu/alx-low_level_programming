1307b4ef468ea1944ea0b1dc4ec5b197fc9c7319
Aliyu Ahmad <maidubu@gmail.com>
2023-02-16 15:22:02 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A C program that can print line with puts function
+ * Return:0(Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
