b78ddf23ea78f1abe05bcec001075d2d73d95983
Aliyu Ahmad <maidubu@gmail.com>
2023-02-17 14:33:05 -0800

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Get a ramdom number and print the number
+ * if it is positive, negetive, or zero
+ * Return: 0(Success)
+ */
+int main(void)
+{
+	int n;
+	
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%i is positive\n, n");
+	else if (n < 0)
+		printf("%i is negative\n, n");
+	else
+		printf("%i is zero\n, n");
+	return (0);
+}
+
+
