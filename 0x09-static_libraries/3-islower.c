f74e8d13989d36352e9d6a643d7541c719fed4c9
Aliyu Ahmad <maidubu@gmail.com>
2023-03-11 13:55:20 -0800

    Done and Dusted

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _islower - program that checks for lowercase character.
+ * @c: is the character to be checked
+ * Return: 1 if c is lowercase, otherwise 0
+**/
+int _islower(int c)
+{
+	return ((c >= 'a' && c <= 'z') ? 1 : 0);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _abs - A program that  computes the absolute value of an integer.
+ * @n: number
+ * Return: absolute value
+**/
+int _abs(int n)
+{
+	return (n < 0 ? n * -1 : n);
+}
